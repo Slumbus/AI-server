68af35058a67139f544f20119c54c1ff71b8bcc6
twosky0202 <leehaneul0202@naver.com>
2024-05-27 08:54:09 +0900

    feat: SPICE를 사용한 피치 감지

--- slumbus_ai/app.py
+++ slumbus_ai/app.py
@@ -1,6 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify, send_file, render_template
+from spice import process_audio
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_pybo():
-    return 'Hello, Pybo!'
+def index():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # Check if the post request has the file part
+    if 'file' not in request.files:
+        return jsonify(error="No file part"), 400
+
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an empty file without a filename
+    if file.filename == '':
+        return jsonify(error="No selected file"), 400
+
+    # If the file exists and is valid
+    if file:
+        # Save the file to the server
+        filename = 'recorded_audio.wav'
+        file.save(filename)
+
+        # Process the audio file
+        midi_file = process_audio(filename)
+
+        # Return the processed MIDI file to the user
+        return send_file(midi_file, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
