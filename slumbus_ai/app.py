11b122d00f45f575218a70055485b1bd4f15f15e
ninano05 <ohjy0530@gmail.com>
2024-05-27 02:53:23 +0900

    feat: musicGen initial-setting

--- slumbus_ai/app.py
+++ slumbus_ai/app.py
@@ -1,5 +1,11 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restx import Api, Resource
+from musicGen.musicGen import MusicGen
+
+app = Flask(__name__) #FLASK 객체 선언, 애플리케이션 패키지 이름 넣기
+api = Api(app) #Flask 객체에 APi 객체 등록
+
+api.add_namespace(MusicGen, '/test')
 
 @app.route('/')
 def hello_pybo():
